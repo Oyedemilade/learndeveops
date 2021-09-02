c80222a5e1b57b2ec9e61bcbacb60e14b2d19650
Oyedemilade <45632737+Oyedemilade@users.noreply.github.com>
2021-09-02 08:33:18 +0100

    Add files via upload

---
+++ terraform/terraform-manifests-aks-custom-vnet/03-resource-group.tf
@@ -0,0 +1,7 @@
+# Terraform Resource to Create Azure Resource Group with Input Variables defined in variables.tf
+resource "azurerm_resource_group" "titans-RG1" {
+  name = "${var.resource_group_name}-${var.environment}"
+  location = var.location
+}
+
+
